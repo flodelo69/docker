236ad6c415c9893046ee0deaa973f58f070cabf5
flodelo69 <flodelo69@gmail.com>
2015-05-16 15:39:05 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,11 +3,13 @@
 # Create the directory needed to run the sshd daemon
 mkdir /var/run/sshd 
 
-#add user ynov
-useradd -m -d /home/ynov -p ynov-aix ynov
+# Add user ynov
+DOCKER_PASSWORD=`pwgen -c -n -1 12`
+echo User: ynov Password: $DOCKER_PASSWORD
+DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
+useradd -m -d /home/ynov -p $DOCKER_ENCRYPYTED_PASSWORD ynov
 sed -Ei 's/adm:x:4:/ynov:x:4:ynov/' /etc/group
 adduser ynov sudo
-chsh -s /bin/bash ynov
 
 # Copy the config files into the docker directory
 cd /src/config/ && sudo -u ynov cp -R .[a-z]* [a-z]* /home/ynov/
