719188d37e63217889b1abddf18ae6107526730d
flodelo69 <flodelo69@gmail.com>
2015-05-16 16:45:42 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,17 +5,17 @@ mkdir /var/run/sshd
 
 # Add docker user and generate a random password with 12 characters that includes at least one capital letter and number.
 DOCKER_PASSWORD=`pwgen -c -n -1 12`
-echo User: docker Password: $DOCKER_PASSWORD
+echo User: ynov Password: $DOCKER_PASSWORD
 DOCKER_ENCRYPYTED_PASSWORD=`perl -e 'print crypt('"$DOCKER_PASSWORD"', "aa"),"\n"'`
-useradd -m -d /home/docker -p $DOCKER_ENCRYPYTED_PASSWORD docker
-sed -Ei 's/adm:x:4:/docker:x:4:docker/' /etc/group
-adduser docker sudo
+useradd -m -d /home/ynov -p $DOCKER_ENCRYPYTED_PASSWORD ynov
+sed -Ei 's/adm:x:4:/ynov:x:4:ynov/' /etc/group
+adduser ynov sudo
 
 # Set the default shell as bash for docker user.
-chsh -s /bin/bash docker
+chsh -s /bin/bash ynov
 
 # Copy the config files into the docker directory
-cd /src/config/ && sudo -u docker cp -R .[a-z]* [a-z]* /home/docker/
+cd /src/config/ && sudo -u docker cp -R .[a-z]* [a-z]* /home/ynov/
 
 # Start the ssh service
 /usr/sbin/sshd -D
