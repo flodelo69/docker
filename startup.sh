06295a0e10bfd2c618ec0bbdc192e958fbc9df62
flodelo69 <flodelo69@gmail.com>
2015-05-16 15:05:29 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,8 +17,5 @@ chsh -s /bin/bash docker
 # Copy the config files into the docker directory
 cd /src/config/ && sudo -u docker cp -R .[a-z]* [a-z]* /home/docker/
 
-# restarts the xdm service
-/etc/init.d/xdm restart
-
 # Start the ssh service
 /usr/sbin/sshd -D
