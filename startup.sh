299e1d45065c1c7f1a207e016b17f3e31e772687
flodelo69 <flodelo69@gmail.com>
2015-05-16 15:51:36 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,6 +11,9 @@ useradd -m -d /home/ynov -p $DOCKER_ENCRYPYTED_PASSWORD ynov -c 'parametre un Cr
 sed -Ei 's/adm:x:4:/ynov:x:4:ynov/' /etc/group
 adduser ynov sudo
 
+# Set the default shell as bash for docker user.
+chsh -s /bin/bash ynov
+
 # Copy the config files into the docker directory
 cd /src/config/ && sudo -u ynov cp -R .[a-z]* [a-z]* /home/ynov/
 
