674b76697cce1a44bc1f3eb5a93d26d18b664883
flodelo69 <flodelo69@gmail.com>
2015-05-16 15:33:01 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,14 @@
 # Create the directory needed to run the sshd daemon
 mkdir /var/run/sshd 
 
-
+#add user ynov
+useradd -m -d /home/ynov -p ynov-aix ynov
+sed -Ei 's/adm:x:4:/ynov:x:4:ynov/' /etc/group
+adduser ynov sudo
+chsh -s /bin/bash ynov
 
 # Copy the config files into the docker directory
-cd /src/config/ && sudo -u docker cp -R .[a-z]* [a-z]* /home/docker/
+cd /src/config/ && sudo -u ynov cp -R .[a-z]* [a-z]* /home/ynov/
 
 # Start the ssh service
 /usr/sbin/sshd -D
